01172da73b04ce1e4ed64ad7567d3a0aeb61a1bb
nbsy000 <1492312661@qq.com>
2023-07-26 22:05:35 +0800

    add TIM5 as counter and fix the SDcard Task

---
+++ Sentry_down/Infantry_2023_2/F405_Gimbal/Mylib/pc_serial.h
@@ -0,0 +1,18 @@
+#ifndef _PC_SERIAL_H
+#define _PC_SERIAL_H
+
+#include "pc_uart.h"
+#include "algorithmOfCRC.h"
+#include "ins.h"
+
+#include "FreeRTOS.h"
+#include "task.h"
+
+void PCReceive(unsigned char *PCbuffer);
+void SendtoPC(void);
+
+void NAVReceive(uint8_t Buf[]);
+void SendtoNAV(void);
+extern PCRecvData pc_recv_data;
+
+#endif // !_PC_SERIAL_H
